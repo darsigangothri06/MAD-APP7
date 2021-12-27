6db472ad2446aac45fbdf117fd28481a335950f8
darsigangothri06 <darsigangothri@gmail.com>
2021-12-27 14:22:19 +0530

    Add: App13 Dynamic Call

---
+++ app/src/main/java/com/example/app13/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.app13;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText phone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        phone = findViewById(R.id.phone);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phone.getText().toString()));
+
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                    return;
+                startActivity(callIntent);
+            }
+        });
+    }
+}
