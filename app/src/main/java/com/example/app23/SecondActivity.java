bcf8bf80c34a2179398d28f10e23f9db9ff445c5
darsigangothri06 <darsigangothri@gmail.com>
2021-12-29 14:07:25 +0530

    Add: App23 Triple Screen Display Example

---
+++ app/src/main/java/com/example/app23/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.app23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent J = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(J);
+            }
+        });
+    }
+}
